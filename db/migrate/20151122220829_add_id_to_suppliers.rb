22eff3296828144e5e450dcc04fa13e3c4d9c69f
ACLTC <acltc@ACLTCs-MacBook-Air-27.local>
2015-11-29 10:29:36 -0600

    changed supplier id migrations to numeric

--- db/migrate/20151122220829_add_id_to_suppliers.rb
+++ db/migrate/20151122220829_add_id_to_suppliers.rb
@@ -1,5 +1,5 @@
 class AddIdToSuppliers < ActiveRecord::Migration
   def change
-    add_column :suppliers, :supplier_id, 'decimal USING CAST(supplier_id AS decimal)'
+    add_column :suppliers, :supplier_id, 'numeric USING CAST(supplier_id AS numeric)'
   end
 end

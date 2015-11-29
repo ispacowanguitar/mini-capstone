be51ab757844f0bb01af1474cfe3be8d865d6ef8
ACLTC <acltc@ACLTCs-MacBook-Air-27.local>
2015-11-29 10:27:56 -0600

    migrations change in supplierID to decimal

--- db/migrate/20151122220829_add_id_to_suppliers.rb
+++ db/migrate/20151122220829_add_id_to_suppliers.rb
@@ -1,5 +1,5 @@
 class AddIdToSuppliers < ActiveRecord::Migration
   def change
-    add_column :suppliers, :supplier_id, 'integer USING CAST(supplier_id AS integer)'
+    add_column :suppliers, :supplier_id, 'decimal USING CAST(supplier_id AS decimal)'
   end
 end

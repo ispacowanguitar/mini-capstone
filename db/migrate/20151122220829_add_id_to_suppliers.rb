fb90bf98c20fb5751df8479852687848af82edca
ACLTC <acltc@ACLTCs-MacBook-Air-27.local>
2015-11-29 10:26:01 -0600

    fixed supplierId in migrations

--- db/migrate/20151122220829_add_id_to_suppliers.rb
+++ db/migrate/20151122220829_add_id_to_suppliers.rb
@@ -1,5 +1,5 @@
 class AddIdToSuppliers < ActiveRecord::Migration
   def change
-    add_column :suppliers, :supplier_id, 'integer USING CAST(price AS integer)'
+    add_column :suppliers, :supplier_id, 'integer USING CAST(supplier_id AS integer)'
   end
 end
